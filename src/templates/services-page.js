8ead2ff718fcf7749e0c36e5975f15b2f7b1f0b2
izabelle wilding <izabellewilding@gmail.com>
2020-03-05 10:47:15 +0000

    reactivated about text on img for mob

--- src/templates/services-page.js
+++ src/templates/services-page.js
@@ -110,7 +110,7 @@ export const ServicesPageTemplate = ({ title, serviceTiles, image }) => {
               }}
             >
               {" "}
-              <h3 className="is-sand feature-text margin-bottom is-hidden-mobile">
+              <h3 className="is-sand feature-text margin-bottom ">
                 {" "}
                 Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
                 eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
